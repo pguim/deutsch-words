38754b947d5d2f4269e5368eddf850d11b5626b8
guimmie <pguimera@gmail.com>
2025-10-16 15:25:01 +0200

    Start creating time range selector

--- src/components/StatsReviews.jsx
+++ src/components/StatsReviews.jsx
@@ -9,22 +9,26 @@ import {
   Legend,
 } from 'chart.js'
 import Card from './Card'
+import { useState } from 'react'
 
 ChartJS.register(LineElement, CategoryScale, LinearScale, PointElement, Tooltip, Legend)
 
 export default function StatsReviews ({ reviews }) {
-  const reviewsData = [5, 1, 8, 4, 3, 6, 9]
+
+  const [timeRange, setTimeRange] = useState(0)
+  const [reviewsData, setReviewsData] = useState([5, 1, 8, 4, 3, 6, 9])
+  const [reviewsLabels, setReviewsLabels] = useState(['Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb', 'Dom'])
 
   const data = {
-    labels: ['Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb', 'Dom'],
+    labels: reviewsLabels,
     datasets: [
       {
         label: 'Revisiones',
         data: reviewsData,
         fill: false,
         borderColor: 'rgba(34, 197, 94, .8)',
-        backgroundColor: 'rgba(34, 197, 94, 0.2)',
-        tension: 0.4, // 🔹 controla la suavidad (0 = recto, 1 = muy curvo)
+        backgroundColor: 'rgba(34, 197, 94, 0.6)',
+        tension: .4,
         pointBackgroundColor: 'rgba(34, 197, 94, .4)',
         pointStyle: 'rectRounded',
         pointRadius: 6,
@@ -52,7 +56,6 @@ export default function StatsReviews ({ reviews }) {
     },
   }
 
-
   return (
     <Card noCenter={true}>
       <h3 className="font-semibold mb-2">Revisiones</h3>
@@ -60,6 +63,26 @@ export default function StatsReviews ({ reviews }) {
         <div className="w-full h-64 flex justify-center my-12">
           <Line data={data} options={options} />
         </div>
+      </div>
+      <div className="relative bg-gray-100 border-slate-300 border-1 rounded-lg p-1 flex justify-between w-full">
+        <div
+          className={`absolute top-1 bottom-1 w-1/3 bg-blue-500 opacity-90 rounded-md transition-all duration-300`}
+          style={{
+            width: "calc((100% - 0.5rem * 2) / 3)", // restamos el padding horizontal (2 × 0.25rem × 2)
+            left: `calc(${timeRange * 100 / 3}% + ${timeRange * 1 / 3}rem)` // ajustamos la posición
+          }}
+        ></div>
+
+        {["Semana", "Mes", "Año"].map((label, i) => (
+          <button
+            key={i}
+            onClick={() => setTimeRange(i)}
+            className={`cursor-pointer relative z-10 flex-1 text-center py-2 font-medium transition-colors duration-300 ${timeRange === i ? "text-white" : "text-gray-600"
+              }`}
+          >
+            {label}
+          </button>
+        ))}
       </div>
     </Card>
   )
